d051756f9807eea3175d987da604fdca13eae95a
Mario Umaña <mburgosgit003@gmail.com>
2023-10-06 10:00:41 -0600

    Add: Report file of top 10 Tech Crunch News and succesfully sent to our email

---
+++ functions.py
@@ -0,0 +1,18 @@
+import smtplib as smtp
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "marioua289@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "marioua289@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtp.SMTP_SSL(host=host, port=port, context=context) as server: 
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 #! news-api-email\venv\Scripts\python.exe
 import requests
+from functions import send_email
 
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8c95c9a745c14bcfbece40161e9694ed"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8c95c9a"\
+      "745c14bcfbece40161e9694ed"
 my_api_key = "8c95c9a745c14bcfbece40161e9694ed"
+FILEPATH = './text-files/report.txt'
 
 # Make a request
 request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
+titles = []
+descriptions = []
 
-# Acces the article, titles and description
-for index, article in enumerate(content["articles"],1):
-    print(f"{index}- Title: {article['title']}")
-    print(f"Description: {article['description']}")
+for article in content["articles"]:
+    titles.append(article["title"])
+    descriptions.append(article["description"])
+
+articles = list(zip(titles, descriptions))
+
+with open(FILEPATH,'w') as file:
+    article_number = 1
+    for article in articles:
+        header = f"{str(article_number)} - Title: {article[0]}"
+        description = f"Description: {article[1]}"
+        file.write(header + "\n" + description  + "\n" + "\n")
+        article_number += 1
+        
+with open(FILEPATH,'r') as report:
+    articles_report = report.read()
+
+complete_message = f"""\
+Subject: Tech Crunch News
+
+{articles_report}
+"""
+encoded_message = complete_message.encode('utf-8')
+send_email(encoded_message)
