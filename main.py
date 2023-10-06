ae2e9734bbd7a034f52da1070d5ab54ee73c42f0
Mario Umaña <mburgosgit003@gmail.com>
2023-10-06 01:58:21 -0600

    Accessing the articles and displaying them in terminal

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #! news-api-email\venv\Scripts\python.exe
 import requests
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-06&sortBy=publishedAt&apiKey=8c95c9a745c14bcfbece40161e9694ed"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=8c95c9a745c14bcfbece40161e9694ed"
 my_api_key = "8c95c9a745c14bcfbece40161e9694ed"
 
+# Make a request
 request = requests.get(url)
-content = request.text
-print(content)
+# Get a dictionary with data
+content = request.json()
+
+# Acces the article, titles and description
+for index, article in enumerate(content["articles"],1):
+    print(f"{index}- Title: {article['title']}")
+    print(f"Description: {article['description']}")
